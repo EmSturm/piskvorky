20d4a0a7c24d79ac5db8b4675e9764a94965f466
EmSturm <emsturmova@gmail.com>
2023-10-05 15:25:06 +0200

    Úprava znázoenění, kdo právě hraje

--- hra.js
+++ hra.js
@@ -6,16 +6,21 @@ const makeCircle = (event) => {
         event.target.classList.remove('board__field--cross');
         event.target.classList.add('board__field--circle');
         currentPlayer = 'cross';
+        document.querySelector('#play__sign').src = 'circle.svg'
     } else  {
 
         event.target.classList.remove('board__field--circle');
         event.target.classList.add('board__field--cross');
         currentPlayer = 'circle'; 
+        document.querySelector('#play__sign').src = 'cross.svg'
     }
 }
-       
-    
 
+// currentPlayer = 'cross';
+
+
+// document.querySelector('#play__sign').addEventListener('click', makePlayerSign);
+       
 document.querySelector('#button__one').addEventListener('click', makeCircle)
 document.querySelector('#button__two').addEventListener('click', makeCircle)
 document.querySelector('#button__three').addEventListener('click', makeCircle)
@@ -26,3 +31,5 @@ document.querySelector('#button__seven').addEventListener('click', makeCircle)
 document.querySelector('#button__eight').addEventListener('click', makeCircle)
 document.querySelector('#button__nine').addEventListener('click', makeCircle)
 document.querySelector('#button__ten').addEventListener('click', makeCircle)
+
+
