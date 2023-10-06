7efbe1555bcb7f0346bdae67e4837b875366fa63
EmSturm <emsturmova@gmail.com>
2023-10-06 10:08:00 +0200

    Finální verze třetího úkolu v JS

--- hra.js
+++ hra.js
@@ -7,12 +7,16 @@ const makeCircle = (event) => {
         event.target.classList.add('board__field--circle');
         currentPlayer = 'cross';
         document.querySelector('#play__sign').src = 'circle.svg'
+        document.querySelector('#play__sign').alt = 'právě hraje kroužek'
+        document.querySelectorAll('.button').disabled = true
     } else  {
 
         event.target.classList.remove('board__field--circle');
         event.target.classList.add('board__field--cross');
         currentPlayer = 'circle'; 
         document.querySelector('#play__sign').src = 'cross.svg'
+        document.querySelector('#play__sign').alt = 'právě hraje křížek'
+        document.querySelectorAll('.button').disabled = true
     }
 }
 
